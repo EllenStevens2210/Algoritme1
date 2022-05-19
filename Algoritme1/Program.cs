aa5d4fa9ad4be9cbb111d54c1d5f65832d4391ef
merelelings <82808651+merelelings@users.noreply.github.com>
2022-05-19 13:55:44 +0200

    Versie 1 algoritme

--- Algoritme1/Program.cs
+++ Algoritme1/Program.cs
@@ -3,71 +3,129 @@ using System;
 
 class GFG
 {
-
-    // Function to find the minimum weight Hamiltonian Cycle
-    static int tsp(int[,] graph, bool[] v, int currPos,
-            int n, int count, int cost, int ans)
-    {
-
-        // If last node is reached and it has a link
-        // to the starting node i.e the source then
-        // keep the minimum value out of the total cost
-        // of traversal and "ans"
-        // Finally return to check for more possible values
-        if (count == n && graph[currPos, 0] > 0)
-        {
-            ans = Math.Min(ans, cost + graph[currPos, 0]);
-            return ans;
-        }
-
-        // BACKTRACKING STEP
-        // Loop to traverse the adjacency list
-        // of currPos node and increasing the count
-        // by 1 and cost by graph[currPos,i] value
-        for (int i = 0; i < n; i++)
-        {
-            if (v[i] == false && graph[currPos, i] > 0)
-            {
-
-                // Mark as visited
-                v[i] = true;
-                ans = tsp(graph, v, i, n, count + 1,
-                    cost + graph[currPos, i], ans);
-
-                // Mark ith node as unvisited
-                v[i] = false;
-            }
-        }
-        return ans;
-    }
-
-    // Driver code
+   // Driver code
     static void Main()
     {
         // n is the number of nodes i.e. V
-        int n = 4;
-
-        int[,] graph = {
-        { 0, 10, 15, 20 },
-        { 10, 0, 35, 25 },
-        { 15, 35, 0, 30 },
-        { 20, 25, 30, 0 }
+        int Leverancier = 2;
+        int Hubs = 1;
+        int Klant = 3;
+        int count = 0;
+        // Distance matrix: Leverancier, hubs, klanten
+        int[,] dist = {
+        { 0,100,70,110,80,170 },
+        { 100,0,90,130,30,160 },
+        { 70,90,0,70,60,130 },
+        { 110,130,70,0,100,170},
+        { 80,30,60,100,0,150 },
+        { 170,160,130,170,150 ,0}
     };
+
+        // Koerier afstand - tot leverancier + hub + klant
+        int[] Koerier = { };
+
+        // welke klant bestelt bij leverancier
+        int[] L1 = { 1,2,3 };
+        int[] L2 = { 1,3 };
 
         // Boolean array to check if a node
         // has been visited or not
-        bool[] v = new bool[n];
+        bool[] v = new bool[Leverancier+Hubs+Klant];
+        bool[] h = new bool[Klant];
+        int[] list = new int[Leverancier + Hubs + Klant];
 
-        // Mark 0th node as visited
-        v[0] = true;
-        int ans = int.MaxValue;
+        for (int i = 0; i< Klant; i++)
+        {
+            if(L1.Contains(i+1) && L2.Contains(i+1))
+            {
+                h[i] = true;
+            }
+        }
+        // kies welke leverancier als eerste te bezoeken
+        if (L1.Length >= L2.Length){
+            v[0]=true;
+            list[count] = 0;
+            count++;
+        }
+        else{
+            v[1] = true;
+            list[count] = 1;
+            count++;
+        }
 
-        // Find the minimum weight Hamiltonian Cycle
-        ans = tsp(graph, v, 0, n, 1, 0, ans);
+        // Check heeft een van de klanten alle spullen al bij koerier liggen
+        for (int i = 0; i < Klant; i++)
+        {
+            if (L1.Contains(i + 1) == (L1.Length >= L2.Length) && L2.Contains(i + 1) == (L1.Length < L2.Length)) {
+                bool c = dist[0,i+Hubs+Leverancier] < dist[0, 1];
+                Console.WriteLine(c);
+                if (c == true)
+                {
+                    list[count] = i + Leverancier + Hubs ;
+                    count++;
+                    v[i + Leverancier + Hubs ] = true;
+                }
+            }
+        }
+        if (v[0] == true)
+        {
+            list[count] = 1; 
+            count++;
+            v[1] = true;
+        }
+        else
+        {
+            list[count] = 0;
+            count++;
+            v[0] = true;
+        }
 
-        // ans is the minimum weight Hamiltonian Cycle
-        Console.Write(ans);
+        // IN DIT GEVAL 1 KOERIER HUB NIET GEBRUIKT
+        v[2] = true;
 
+        // route langs overige klanten - NU SUPER SPECIFIEK, WERKT BIJVOORBEELD NIET ALS ER NIET 2 V OP FALSE STAAN
+        for (int i = 0; i < Leverancier + Hubs + Klant; i++)
+        {
+            if (v[i] == false)
+            {
+                for (int j = 0; j < Leverancier + Hubs + Klant; j++)
+                {
+                    if(v[j] == false && i!= j){
+                        //if (dist[count, i] + dist[list[0],j] < dist[count, j] + dist[list[0], j]) // als tour belangrijk is (terug naar begin)
+                        if (dist[count, i] + dist[i, j] < dist[count, j] + dist[j,i])
+                        {
+                            v[i] = true;
+                            list[count] = i;
+                            list[count + 1] = j;
+                        }
+                        else
+                        {
+                            v[j] = true;
+                            list[count] = j;
+                            list[count + 1] = i;
+                        }
+                    }
+                }
+            }
+        }
+
+        // HOE PRINT JE EEN ARRAY?
+        for (int i=0; i < list.Length-1; i++) {
+            Console.Write(list[i]);
+        }
+        Console.WriteLine("\n");
+        // CHECK TOEVOEGEN ALLES VAN FALSE AF
+
+        // TODO GENERIEKER MAKEN MET n IPV NU VAST VOOR AANTAL LEVERANCIERS BV
+
+
+        int distance=0;
+        for (int i=0; i < list.Length-2; i++)
+        {
+            distance = distance + dist[list[i], list[i + 1]];
+        }
+        
+        Console.WriteLine(distance);
     }
 }
 
